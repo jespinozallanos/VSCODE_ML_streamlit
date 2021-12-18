2572c5c1a6c002f7a5aeb18a9d36615fc85cc872
Julio Espinoza <jespinozallanos@gmail.com>
2021-12-18 00:36:43 -0300

    commit6

--- mean.py
+++ mean.py
@@ -2,8 +2,7 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import sklearn.linear_model
-# import LinearRegression, LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.svm import SVC
 import pickle
 
