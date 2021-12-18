be1192976868a0c9084d1acf1c28cdfaf47e7852
Julio Espinoza <jespinozallanos@gmail.com>
2021-12-17 23:28:00 -0300

    commit 1 desde VS

---
+++ iris_web.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+ #probar streamlit
+ #en terminal:  streamlit hello
+ 
+# extraccion datos
+with open('lin_reg.pkl', 'rb') as li:
+    lin_reg = pickle.load(li)
+
+with open('log_reg.pkl', 'rb') as lo:
+    log_reg = pickle.load(lo)
+
+with open('svc_m.pkl', 'rb') as sv:
+    svc_m = pickle.load(sv)
+
+#funcion para clasificar las plantas: 1 para virginica, 0 para setosa
+def classify(num):
+    if num == 0:
+        return 'setosa'
+    elif num == 1:
+        return 'versicolor'
+    else:
+        return 'virginica'
+
+def main():
+    #titulo
+    st.title('modelamiento de iris por julio')
+    
+    #titulo de sidebar
+    st.sidebar.header('User Input parameters')
+    
+    #funcion para poner parametros en sidebar
+    def user_input_parameters():
+        sepal_length = st.sidebar.slider('sepal lenght', 4.3, 7.9, 5.4)
+        sepal_width = st.sidebar.slider('sepal width', 2.0, 4.4, 3.4)
+        petal_length = st.sidebar.slider('petal lenght', 1.0, 6.9, 1.3)
+        petal_width = st.sidebar.slider('petal width', 0.1, 2.5, 0.2)
+        data = {'sepal_length': sepal_length,
+                'sepal_width': sepal_width,
+                'petal_length': petal_length,
+                'petal_width': petal_width
+                }
+        features = pd.DataFrame(data, index=[0])
+        return features
+    
+    df = user_input_parameters()
+    
+    #escoger modelo preferido
+    option = ['linear Regression', 'logistic regression', 'SVM']
+    model =st.sidebar.selectbox('wich model do you like to use??', option)
+    
+    st.subheader('user input parameters')
+    st.subheader(model)
+    st.write(df)
+    
+    
+    if st.button('RUN'):
+        if model == 'linear Regression':
+            st.success(classify(lin_reg.predict(df)))
+        elif model == 'logistic regression':
+            st.success(classify(log_reg.predict(df)))
+        else:
+            st.success(classify(svc_m.predict(df)))
+    
+    
+#metodo de entrada
+if __name__ == '__main__':
+    main()
+    
+    
+# para correr en powershell:
+# streamlit run iris_web.py
---
+++ mean.py
@@ -0,0 +1,39 @@
+#importar
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC
+import pickle
+
+#cargar datos
+
+iris=datasets.load_iris()
+X=iris.data
+y=iris.target
+#separar datos en entrenamiento y prueba
+x_train, x_test, y_train, y_test = train_test_split(X,y)
+
+lin_reg = LinearRegression()
+log_reg = LogisticRegression()
+
+svc_m = SVC()
+
+#entrenamiento modelos
+lin_regr = lin_reg.fit(x_train,y_train)
+log_regr = log_reg.fit(x_train,y_train)
+svc_mo = svc_m.fit(x_train,y_train)
+
+#guardar modelos en archivo pickle
+with open('lin_reg.pkl', 'wb') as li:
+    pickle.dump(lin_regr, li)
+with open('log_reg.pkl', 'wb') as lo:
+    pickle.dump(log_regr, lo)
+with open('svc_m.pkl', 'wb') as sv:
+    pickle.dump(svc_m, sv)
+
+#con ls en el terminal puedo ver los archivos creados pickle
+# siempre seleccionar interprete:  shift+control+P-->python:select interpreter-->Base
+#usar powersheel (terminal)
+
+
